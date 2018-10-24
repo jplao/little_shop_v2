eb55bac73e80bcb6a0c52169aec834eda59cc445
Ryan McNeil <ryan.mcneil288@gmail.com>
2018-10-24 16:34:43 -0600

    test for user registration

---
+++ spec/features/user_can_register_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+describe 'user registration' do
+  it 'anonymous visitor' do
+    user = build(:user)
+
+    visit root_path
+
+    click_on 'Register'
+
+    expect(current_path).to eq(register_path)
+    fill_in :user_name, with: user.name
+    fill_in :user_street_address, with: user.street_address
+    fill_in :user_city, with: user.city
+    fill_in :user_state, with: user.state
+    fill_in :user_zip, with: user.zip
+    fill_in :user_email, with: user.email
+    fill_in :user_password, with: user.password
+    fill_in :password_confirmation, with: user.password
+    click_on "Create User"
+
+    expect(current_path).to eq(profile_path(user))
+    expect(page).to have_content(user.name)
+    expect(page).to have_content(user.street_address)
+    expect(page).to have_content(user.city)
+    expect(page).to have_content(user.state)
+    expect(page).to have_content(user.zip)
+    expect(page).to have_content(user.email)
+  end
+end
