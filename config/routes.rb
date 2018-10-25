9cd3b98ccbce7074914cc609b8c3ad3fb5f5aada
Preston Jarnagin <prstnjarnagin@gmail.com>
2018-10-25 11:55:10 -0600

    Routing stuff

--- config/routes.rb
+++ config/routes.rb
@@ -9,13 +9,13 @@ Rails.application.routes.draw do
   get '/register', to: 'users#new'
   post '/register', to: 'users#create'
 
-  get '/profile/:id', to: 'users#show'
+  get '/profile', to: 'users#show'
   get '/profile/edit', to: 'users#edit'
   put '/profile/edit', to: 'users#update'
   get '/profile/orders', to: 'orders#index'
   delete '/profile/orders/:id', to: 'orders#destroy'  #should destroy be here or within resources :orders?
 
-  get '/dashboard', to: 'users#show'
+  get '/dashboard', to: 'dashboard#show'
   get '/dashboard/items', to: 'items#index'
   get '/dashboard/items/new', to: 'items#new'
 
@@ -23,13 +23,13 @@ Rails.application.routes.draw do
 
   resources :carts, only: [:create] #revisit after cart class
   get '/cart', to: 'carts#index'
-  
-  resources :users, only: [:index, :show] #move to admin?
 
   resources :items, only: [:show, :index, :new, :create, :edit, :update, :destroy]
 
   get '/merchants', to: 'users#index'
   get '/merchants/:id', to: 'users#show'
+
+  resources :users, only: [:show]
 
   namespace :admin do
     resources :users, only: [:index, :update, :destroy] #add edit here?
