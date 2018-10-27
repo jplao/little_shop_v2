709116a49ef9dbe4ca2fd5e1b7c3153189d7d9fc
Jenn Lao <jenn.p.lao@gmail.com>
2018-10-27 15:11:24 -0600

    adds links to user header

--- spec/features/merchant_dashboard_spec.rb
+++ spec/features/merchant_dashboard_spec.rb
@@ -19,15 +19,19 @@ describe 'Merchant dashboard' do
 
   it 'shows a link to orders when a merchant has orders' do
     visit dashboard_path
-
-    expect(page).to have_link("Orders")
+    within(".dashboard_orders") do
+      expect(page).to have_link("Orders")
+    end
     click_link 'Log Out'
   end
 
   it 'on orders page shows all orders' do
 
     visit dashboard_path
-    click_link "Orders"
+    within(".dashboard_orders") do
+      click_link "Orders"
+    end
+
     expect(current_path).to eq(dashboard_orders_path)
     expect(page).to have_link(@order.id)
     expect(page).to have_content(@order.created_at)
@@ -48,7 +52,10 @@ describe 'Merchant dashboard' do
     @order_4.order_items.create(item: @item_4, item_price: 7.99, item_quantity: 4)
 
     visit dashboard_path
-    click_link "Orders"
+    within(".dashboard_orders") do
+      click_link "Orders"
+    end
+
     expect(current_path).to eq(dashboard_orders_path)
 
     expect(current_path).to eq(dashboard_orders_path)
@@ -66,7 +73,10 @@ describe 'Merchant dashboard' do
     click_button "Log In"
 
     visit dashboard_path
-    expect(page).to_not have_link("Orders")
+
+    within(".dashboard_orders") do
+      expect(page).to_not have_link("Orders")
+    end
     click_link 'Log Out'
   end
 
--- spec/features/user_sees_a_nav_bar_spec.rb
+++ spec/features/user_sees_a_nav_bar_spec.rb
@@ -14,12 +14,27 @@ describe 'Navigation' do
       expect(page).to have_link('Log In')
       expect(page).to have_link('Register')
       expect(page).not_to have_link('My Dashboard')
+      expect(page).not_to have_link("My Profile")
+      expect(page).not_to have_link("My Orders")
     end
 
   end
 
   context 'as a registered user' do
+    it 'can see profile links' do
+      @user = create(:user, role: 0)
+      visit root_path
+      click_link "Log In"
+      fill_in :email, with: @user.email
+      fill_in :password, with: @user.password
+      click_button "Log In"
 
+      expect(page).to have_link("My Profile")
+      expect(page).to have_link("My Orders")
+      expect(page).to have_link("Log Out")
+      expect(page).not_to have_link("My Dashboard")
+      click_link "Log Out"
+    end
   end
 
   context 'as a merchant' do
@@ -32,6 +47,7 @@ describe 'Navigation' do
       click_button "Log In"
 
       expect(page).to have_link('My Dashboard')
+      click_link "Log Out"
     end
   end
 
