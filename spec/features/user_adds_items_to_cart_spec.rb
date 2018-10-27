677265833bdd58e66c07a6780fac963be6d4bc0e
Preston Jarnagin <prstnjarnagin@gmail.com>
2018-10-27 15:17:28 -0600

    Cart empty wip

--- spec/features/user_adds_items_to_cart_spec.rb
+++ spec/features/user_adds_items_to_cart_spec.rb
@@ -43,16 +43,32 @@ describe 'adding items to cart' do
       visit cart_path
       within "#item-#{@item_1.id}" do
         expect(page).to have_content(@item_1.name)
-        expect(page).to have_content(@item_1.merchant)
-        expect(page).to have_content(@item_1.price)
+        expect(page).to have_content(@item_1.user.name)
+        expect(page).to have_content("$#{@item_1.price.round(2)}")
         expect(page).to have_content("Quantity: 1")
       end
       within "#item-#{@item_2.id}" do
         expect(page).to have_content(@item_2.name)
-        expect(page).to have_content(@item_2.merchant)
-        expect(page).to have_content(@item_2.price)
+        expect(page).to have_content(@item_2.user.name)
+        expect(page).to have_content("$#{@item_2.price.round(2)}")
         expect(page).to have_content("Quantity: 2")
       end
+    end
+
+    it 'shows items that have been added to cart' do
+
+      visit item_path(@item_1)
+      click_on('Add to Cart')
+      visit item_path(@item_2)
+      click_on('Add to Cart')
+
+      visit cart_path
+
+      expect(page).to have_button("Empty Cart")
+      click_on "Empty Cart"
+      visit cart_path
+      expect(page).to_not have_content(@item_1.name)
+      expect(page).to_not have_content(@item_2.name)
     end
   end
 end
