a3d85cfe31fe4357f8472e88a9cd913754fe18ec
Preston Jarnagin <prstnjarnagin@gmail.com>
2018-10-26 18:52:30 -0600

    Start merchant dashboard

--- spec/features/merchant_dashboard_spec.rb
+++ spec/features/merchant_dashboard_spec.rb
@@ -2,25 +2,34 @@ require 'rails_helper'
 
 describe 'Merchant dashboard' do
 
-  before(:each) do
+  it 'shows a link to orders when a merchant has orders' do
     merchant = create(:user, role: 1)
     customer = create(:user)
     item = create(:item)
     order = create(:order)
     merchant.items = [item]
-    order.order_items.create(item: item, item_price: 1.99, item_quantity: 1)
+    order.order_items.create(item: item, item_price: 1.99, item_quantity: 10)
     customer.orders = [order]
-
     visit root_path
     click_link "Log In"
     fill_in :email, with: merchant.email
-    fill_in :password, with: customer.password
+    fill_in :password, with: merchant.password
     click_button "Log In"
-  end
 
-  it 'shows a link to orders when a merchant has orders' do
     visit dashboard_path
     expect(page).to have_link("Orders")
+  end
+
+  it 'doesnt show a link to orders when a merchant has no orders' do
+    merchant = create(:user, role: 1)
+    visit root_path
+    click_link "Log In"
+    fill_in :email, with: merchant.email
+    fill_in :password, with: merchant.password
+    click_button "Log In"
+
+    visit dashboard_path
+    expect(page).to_not have_link("Orders")
   end
 
 end
