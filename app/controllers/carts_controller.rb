2cbc236999717eb4e94a79dba0f319e8690f7a60
Bailey Diveley <41691872+bdiveley@users.noreply.github.com>
2018-10-27 13:03:29 -0600

    Update carts_controller.rb

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -5,12 +5,6 @@ class CartsController < ApplicationController
     session[:cart] ||= Hash.new
     session[:cart][item.id] ||= 0
     session[:cart][item.id] += 1
-
-    # if session[:cart][item.id]
-    #   session[:cart][item.id] + 1
-    # else
-    #   session[:cart][item.id] = 1
-    # end
     redirect_to item_path(item), notice: "Item Added to Cart"
   end
 end
