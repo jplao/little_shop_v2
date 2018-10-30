b87bb6436dd52da5be317cef27efe02e379bc998
Preston Jarnagin <prstnjarnagin@gmail.com>
2018-10-30 16:03:22 -0600

    Write class method for top merchants

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,4 +13,24 @@ class Item < ApplicationRecord
     @item.update(inventory_count: new_inventory)
   end
 
+  def self.top_items(limit = 5)
+   select('items.*, sum(order_items.item_quantity) as tot_items')
+     .joins(:order_items)
+     .group(:id, :item_id)
+     .order('tot_items desc')
+     .limit(limit)
+  end
+
+  def self.top_seller_ids(limit = 5)
+   select('items.*, sum(order_items.item_quantity) as tot_items')
+     .joins(:order_items)
+     .group(:user_id, :id, :item_id)
+     .order('tot_items desc')
+     .limit(limit)
+     .uniq
+     .pluck(:user_id)
+  end
+
+
+
 end
--- spec/features/user_sees_items_index_spec.rb
+++ spec/features/user_sees_items_index_spec.rb
@@ -20,4 +20,33 @@ describe 'when any user visits the items index page' do
     expect(page).to have_content(item_2.inventory_count)
     expect(page).to have_content(item_2.price)
   end
+
+  it 'shows statistics' do
+    merch_1, merch_2, merch_3, merch_4, merch_5, merch_6 = create_list(:user, 6, role: 1)
+
+    item_1 = create(:item, user: merch_1)
+    item_2 = create(:item, user: merch_2)
+    item_3 = create(:item, user: merch_3)
+    item_4 = create(:item, user: merch_4)
+    item_5 = create(:item, user: merch_5)
+    item_6 = create(:item, user: merch_6)
+
+    order = create(:order, status: "complete", user: merch_6)
+
+    create(:order_item, order: order, item: item_1, item_quantity: 3)
+    create(:order_item, order: order, item: item_2, item_quantity: 3)
+    create(:order_item, order: order, item: item_3, item_quantity: 4)
+    create(:order_item, order: order, item: item_4, item_quantity: 5)
+    create(:order_item, order: order, item: item_5, item_quantity: 6)
+    create(:order_item, order: order, item: item_6, item_quantity: 1)
+
+    visit items_path
+    within(".top_item")do
+      expect(page).to have_content(item_1.name)
+      expect(page).to have_content(item_2.name)
+      expect(page).to have_content(item_3.name)
+      expect(page).to have_content(item_4.name)
+      expect(page).to have_content(item_5.name)
+    end
+  end
 end
