c0afe624ed39bdf7ba5b7b32e1f0090bbe441e4f
Preston Jarnagin <prstnjarnagin@gmail.com>
2018-10-31 11:01:06 -0600

    Fixing coverage

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,11 +3,11 @@ class Admin::UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
-    if current_user == "merchant"
+    if current_user.role == 1
       redirect_to merchants_path, notice: "User account has been disabled"
     else
       redirect_to "/users?display=all", notice: "User account has been disabled"
-    end 
+    end
   end
 
   private
